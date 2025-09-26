e7330376221598bb8424c11c1aaf3481e32874ef
Sofia Mejia <asofimp@gmail.com>
2025-09-25 19:53:17 -0600

    feat: add get by id

--- internal/modules/cliente/routes.go
+++ internal/modules/cliente/routes.go
@@ -6,4 +6,6 @@ func SetupClienteRoutes(api *mux.Router, handler *ClientHandler) {
 	clienteRouter := api.PathPrefix("/clientes").Subrouter()
 
 	clienteRouter.HandleFunc("", handler.CreateCliente).Methods("POST")
+	clienteRouter.HandleFunc("/{id}", handler.GetClienteByID).Methods("GET")
+
 }
