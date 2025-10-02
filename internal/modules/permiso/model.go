b98e71028eead84f82ef484b8f1f8c531b3f7e89
asofimp <asofimp@gmail.com>
2025-10-01 23:28:51 -0600

    feat: add model

--- internal/modules/permiso/model.go
+++ internal/modules/permiso/model.go
@@ -4,10 +4,10 @@ import "time"
 
 type Permiso struct {
 	IdPermiso         int       `json:"id_permiso" db:"id_permiso" gorm:"primaryKey;autoIncrement"`
-	Nombre            string    ``
-	Accion            string    ``
-	Descripcion       *string   ``
-	Estado            bool      ``
-	FechaCreacion     time.Time ``
-	FechaModificacion time.Time ``
+	Nombre            string    `json:"nombre" db:"nombre" gorm:"type:varchar(100);not null"`
+	Accion            string    `json:"accion" db:"accion" gorm:"type:varchar(100);not null"`
+	Descripcion       *string   `json:"descripcion" db:"descripcion" gorm:"type:text"`
+	Estado            bool      `json:"estado" db:"estado" gorm:"default:true"`
+	FechaCreacion     time.Time `json:"fecha_creacion" db:"fecha_creacion" gorm:"autoCreateTime"`
+	FechaModificacion time.Time `json:"fecha_modificacion" db:"fecha_modificacion" gorm:"autoUpdateTime"`
 }
