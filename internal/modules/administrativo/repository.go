14ee86f516a7dfe928383a77ec14471f035ac2da
asofimp <asofimp@gmail.com>
2025-10-08 01:07:33 -0600

    feat: changes clients

--- internal/modules/administrativo/repository.go
+++ internal/modules/administrativo/repository.go
@@ -1,1 +1,96 @@
 package administrativo
+
+import (
+	"pistolistoBE/internal/common"
+
+	"gorm.io/gorm"
+)
+
+type AdminRepository interface {
+	Create(admin *Administrativo) error
+	GetByID(id uint) (*Administrativo, error)
+	GetAll() ([]*Administrativo, error)
+	Delete(id uint) (string, error)
+	FirstAuth(id uint) (string, error)
+	Update(admin *Administrativo) (string, error)
+}
+
+type adminRepository struct {
+	db *gorm.DB
+}
+
+func NewAdminRepository(db *gorm.DB) AdminRepository {
+	return &adminRepository{db}
+}
+
+func (r *adminRepository) Create(rol *Administrativo) error {
+	return r.db.Create(rol).Error
+}
+
+func (r *adminRepository) GetAll() ([]*Administrativo, error) {
+	var permiso []*Administrativo
+
+	err := r.db.Where("estado = ?", true).Find(&permiso).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return permiso, nil
+}
+
+func (r *adminRepository) GetByID(id uint) (*Administrativo, error) {
+	var admin Administrativo
+	err := r.db.Where("estado = ?", true).First(&admin, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &admin, nil
+}
+
+func (r *adminRepository) Delete(id uint) (string, error) {
+	result := r.db.Model(&Administrativo{}).Where("id_permiso = ?", id).Update("estado", false)
+
+	if result.Error != nil {
+		return common.ERR_DATABASE_ERROR, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return common.ERR_NOT_FOUND, gorm.ErrRecordNotFound
+	}
+
+	return common.SUCCESS_DELETED, nil
+}
+
+func (r *adminRepository) Update(permiso *Administrativo) (string, error) {
+	result := r.db.Model(&Administrativo{}).
+		Where("id_permiso = ? AND estado = ?", permiso.IdAdministrativo, true).
+		Updates(permiso)
+
+	if result.Error != nil {
+		return common.ERR_DATABASE_ERROR, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return common.ERR_NOT_FOUND, gorm.ErrRecordNotFound
+	}
+
+	return common.SUCCESS_UPDATED, nil
+}
+
+func (r *adminRepository) FirstAuth(id uint) (string, error) {
+	result := r.db.Model(&Administrativo{}).
+		Where("id_administrativo = ? AND primer_login = ? AND estado = ?", id, false, true).
+		Update("primer_login", true)
+
+	if result.Error != nil {
+		return common.ERR_DATABASE_ERROR, result.Error
+	}
+
+	// Verificar si se actualizó algún registro
+	if result.RowsAffected == 0 {
+		return common.ERR_NOT_FOUND, result.Error
+	}
+
+	return common.SUCCESS_UPDATED, nil
+}
--- internal/modules/auth/model.go
+++ internal/modules/auth/model.go
@@ -12,3 +12,12 @@ type LogLoginCliente struct {
 	FechaModificacion time.Time `gorm:"column:fecha_modificacion;autoUpdateTime" json:"fecha_modificacion"`
 	FechaCreacion     time.Time `gorm:"column:fecha_creacion;autoCreateTime" json:"fecha_creacion"`
 }
+
+type LogLoginAdmin struct {
+	IdLoginAdmin      uint      `json:"id_login_admin" gorm:"primaryKey;autoIncrement;column:id_login_admin"`
+	IdAdministrativo  uint      `json:"id_administrativo" gorm:"not null;column:id_administrativo;index"`
+	Estado            *bool     `json:"estado,omitempty" gorm:"column:estado;default:true"`
+	Exito             bool      `json:"exito" gorm:"column:exito;not null;default:false"`
+	FechaCreacion     time.Time `json:"fecha_creacion" gorm:"column:fecha_creacion;autoCreateTime"`
+	FechaModificacion time.Time `json:"fecha_modificacion" gorm:"column:fecha_modificacion;autoUpdateTime"`
+}
