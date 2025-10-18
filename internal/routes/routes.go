836761bd7e1ef516e871f0d999f406fb8152453c
GeraSamayoa <gerasamayoa419@gmail.com>
2025-10-18 00:46:20 -0600

    fix service

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -24,8 +24,6 @@ import (
 	"pistolistoBE/internal/modules/pago"
 	"pistolistoBE/internal/modules/producto"
 
-	"pistolistoBE/internal/modules/pago"
-	"pistolistoBE/internal/modules/producto"
 	"pistolistoBE/internal/modules/resenaEmpresa"
 	"pistolistoBE/internal/modules/rol"
 
