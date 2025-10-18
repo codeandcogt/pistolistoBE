a0c3a23e3373db4f9cebf21c7c01348e16449533
César Gerardo Samayoa Ramírez <128975354+GeraSamayoa@users.noreply.github.com>
2025-10-18 01:21:31 -0600

    Feat/gsamayoa (#18)
    * add subCategory and direccion
    
    * add folders for articulo
    
    * feat: action
    
    * Feat: modules permisos - rol - rolpermiso  (#12)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * Feat: cambio de modelo clientes (#13)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * feat: changes clients
    
    * add subCategory and direccion
    
    * add folders for articulo
    
    * methods fix
    
    * fix merge
    
    * add folders for articulo
    
    * Feat: modules permisos - rol - rolpermiso  (#12)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * Feat: JWT admin (#14)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * feat: changes clients
    
    * feat: auth admin
    
    * add subCategory and direccion
    
    * add folders for articulo
    
    * Feat: modules permisos - rol - rolpermiso  (#12)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * add subCategory and direccion
    
    * fix merge
    
    * add folders for articulo
    
    * fix: routes
    
    * fix routes
    
    * add producto
    
    * add producto
    
    * pedido test
    
    * factura sin test
    
    * Feature/epivaral (#17)
    
    * Modulo de carito,carrito item
    
    * Modulo de carrito y carrito item
    
    * MOdulo de resñas de la empresa resenaEmpresa
    
    * feat: changes cors
    
    * bug: cors
    
    * bug: conexion a BE
    
    * fix: cors
    
    * bug
    
    * CORS
    
    * add folders for articulo
    
    * Feat: modules permisos - rol - rolpermiso  (#12)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * Feat: cambio de modelo clientes (#13)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * feat: changes clients
    
    * add subCategory and direccion
    
    * Feat: modules permisos - rol - rolpermiso  (#12)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * Feat: JWT admin (#14)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * feat: changes clients
    
    * feat: auth admin
    
    * add subCategory and direccion
    
    * add folders for articulo
    
    * Feat: modules permisos - rol - rolpermiso  (#12)
    
    * feat: add permiso
    
    * feat: add model
    
    * feat: add dependency
    
    * feat: add rol
    
    * feat: add rolpermiso
    
    * Feat: add modules
    
    * add producto
    
    * add producto
    
    * factura sin test
    
    * fix service
    
    * modify model articulo
    
    ---------
    
    Co-authored-by: asofimp <asofimp@gmail.com>
    Co-authored-by: Sofia Mejia <122583805+asofimp@users.noreply.github.com>
    Co-authored-by: Epivarals <132931169+Enigma1225@users.noreply.github.com>

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -94,6 +94,7 @@ type RouteHandlers interface {
 
 func SetupRoutes(router *mux.Router, handlers RouteHandlers) {
 	router.Use(middleware.Recovery)
+
 	router.Use(middleware.Logger)
 	router.Use(middleware.ContentTypeJSON)
 	// API versioning
