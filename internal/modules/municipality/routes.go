5fbf00d5454123787971551a1e9db13ad16a578f
asofimp <asofimp@gmail.com>
2025-10-23 17:25:57 -0600

    fix: jwtadmin

--- internal/modules/municipality/routes.go
+++ internal/modules/municipality/routes.go
@@ -10,11 +10,11 @@ func SetupMunicipalityRoutes(api *mux.Router, handler *MunicipalityHandler) {
 	municipalityRouter := api.PathPrefix("/municipalities").Subrouter()
 
 	// Rutas protegidas con JWT
+	municipalityRouter.HandleFunc("/all", handler.GetAll).Methods("GET")
 	protected := municipalityRouter.NewRoute().Subrouter()
-	protected.Use(middleware.JWTMiddleware)
+	protected.Use(middleware.AdminJWTMiddleware)
 
 	protected.HandleFunc("", handler.Create).Methods("POST")
-	protected.HandleFunc("/all", handler.GetAll).Methods("GET")
 	protected.HandleFunc("/{id}", handler.GetByID).Methods("GET")
 	protected.HandleFunc("/{id}", handler.Update).Methods("PUT")
 	protected.HandleFunc("/{id}", handler.Delete).Methods("DELETE")
