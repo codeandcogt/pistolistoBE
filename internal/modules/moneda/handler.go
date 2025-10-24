af029c59c98270639142b1cb32199dc59829206f
asofimp <asofimp@gmail.com>
2025-10-23 18:33:29 -0600

    fix: string

--- internal/modules/moneda/handler.go
+++ internal/modules/moneda/handler.go
@@ -113,5 +113,5 @@ func (h *MonedaHandler) DeleteMoneda(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	common.SuccessResponse(w, common.SUCCESS_DELETED, nil, common.HTTP_OK)
+	common.SuccessResponse(w, common.SUCCESS_DELETED, common.SUCCESS_DELETED, common.HTTP_OK)
 }
