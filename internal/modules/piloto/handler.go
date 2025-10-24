06fab78cce4c84d5b0936117cee2fee2905c1cef
asofimp <asofimp@gmail.com>
2025-10-24 12:45:34 -0600

    feat: add changes

--- internal/modules/piloto/handler.go
+++ internal/modules/piloto/handler.go
@@ -136,5 +136,5 @@ func (h *PilotoHandler) DeletePiloto(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	common.SuccessResponse(w, common.SUCCESS_DELETED, nil, common.HTTP_OK)
+	common.SuccessResponse(w, common.SUCCESS_DELETED, common.SUCCESS_DELETED, common.HTTP_OK)
 }
